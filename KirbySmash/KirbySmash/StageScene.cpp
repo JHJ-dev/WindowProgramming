3924119272de665f95bcef53587be193241ef4fa
JHJ-dev <wjs3662@gmail.com>
2021-06-09 09:10:18 +0900

    카메라, 캐릭터 이동

--- KirbySmash/KirbySmash/StageScene.cpp
+++ KirbySmash/KirbySmash/StageScene.cpp
@@ -36,13 +36,162 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	HDC hdc;
+	HDC hdc, memdc1, memdc2;
+	static HBITMAP hBitmap;
+	static HBITMAP oldBitmap1, oldBitmap2;
+	static RECT crt;
+
 	PAINTSTRUCT ps;
+	BITMAP bmp;
 
 	switch (uMsg) {
+	case WM_CREATE:
+		GetClientRect(hwnd, &crt);
+		//비트맵 로드
+		//스테이지 배경
+		stageBackground.hBitmap = (HBITMAP)LoadImage(g_hInst, "stageBackground.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		GetObject(stageBackground.hBitmap, sizeof(BITMAP), &bmp);
+		stageBackground.bitmapSize = { bmp.bmWidth, bmp.bmHeight };
+		//스테이지1 버튼
+		stageButton[0].hBitmap = (HBITMAP)LoadImage(g_hInst, "stage1.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		GetObject(stageButton[0].hBitmap, sizeof(BITMAP), &bmp);
+		stageButton[0].bitmapSize = { bmp.bmWidth, bmp.bmHeight };
+		stageButton[0].drawPos = { 100, 450 };
+		stageButton[0].centerPos = { stageButton[0].drawPos.x + bmp.bmWidth / 2, stageButton[0].drawPos.y + bmp.bmHeight / 2 };
+		//스테이지2 버튼
+		stageButton[1].hBitmap = (HBITMAP)LoadImage(g_hInst, "stage2.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		GetObject(stageButton[1].hBitmap, sizeof(BITMAP), &bmp);
+		stageButton[1].bitmapSize = { bmp.bmWidth, bmp.bmHeight };
+		stageButton[1].drawPos = { 300, 400 };
+		stageButton[1].centerPos = { stageButton[1].drawPos.x + bmp.bmWidth / 2, stageButton[1].drawPos.y + bmp.bmHeight / 2 };
+		//스테이지3 버튼
+		stageButton[2].hBitmap = (HBITMAP)LoadImage(g_hInst, "stage3.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		GetObject(stageButton[2].hBitmap, sizeof(BITMAP), &bmp);
+		stageButton[2].bitmapSize = { bmp.bmWidth, bmp.bmHeight };
+		stageButton[2].drawPos = { 520, 395 };
+		stageButton[2].centerPos = { stageButton[2].drawPos.x + bmp.bmWidth / 2, stageButton[2].drawPos.y + bmp.bmHeight / 2 };
+		//커비
+		kirby.hBitmap = (HBITMAP)LoadImage(g_hInst, "kirby.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		GetObject(kirby.hBitmap, sizeof(BITMAP), &bmp);
+		kirby.bitmapSize = { bmp.bmWidth, bmp.bmHeight };
+		kirby.centerPos = { stage12[0].x, stage12[0].y - bmp.bmWidth / 2 };
+		kirby.drawPos = { kirby.centerPos.x - bmp.bmWidth / 2, kirby.centerPos.y - bmp.bmHeight / 2 };
+
+		nowStage = 0;
+		SetTimer(hwnd, 1, 100, NULL);
+		break;
+
+	case WM_TIMER:
+		hdc = GetDC(hwnd);
+		if (hBitmap == NULL) hBitmap = CreateCompatibleBitmap(hdc, crt.right, crt.bottom);
+		memdc1 = CreateCompatibleDC(hdc);
+		memdc2 = CreateCompatibleDC(memdc1);
+		oldBitmap1 = (HBITMAP)SelectObject(memdc1, hBitmap);
+
+		//StretchBlt(출력 DC, 그릴 위치.x, 그릴 위치.y, 그릴 크기.x, 그릴 크기.y, 소스 DC, 메모리DC 위치.x, 메모리DC 위치.y, 가져올 크기.x, 가져올 크기.y, 래스터 연산 방법)
+		//TransparentBlt(출력 DC, 그릴 위치.x, 그릴 위치.y, 그릴 크기.x, 그릴 크기.y, 소스 DC, 메모리DC 위치.x, 메모리DC 위치.y, 가져올 크기.x, 가져올 크기.y, 투명하게 할 색상)
+
+		//스테이지 배경 그리기
+		oldBitmap2 = (HBITMAP)SelectObject(memdc2, stageBackground.hBitmap);
+		StretchBlt(memdc1, 0, 0, SCREENWIDTH, SCREENHEIGHT, memdc2, cx, cy, SCREENWIDTH, SCREENHEIGHT, SRCCOPY);
+
+		//스테이지 버튼 그리기
+		for (i = 0; i < STAGECNT; ++i) {
+			oldBitmap2 = (HBITMAP)SelectObject(memdc2, stageButton[i].hBitmap);
+			TransparentBlt(memdc1, stageButton[i].drawPos.x - cx, stageButton[i].drawPos.y - cy, stageButton[i].bitmapSize.x, stageButton[i].bitmapSize.y, memdc2, 0, 0, stageButton[i].bitmapSize.x, stageButton[i].bitmapSize.y, buttonRGB);
+		}
+
+		//커비 그리기
+		if (wParam == 2) {
+			if (Movedir == 1) {
+				if (moveIndex < POINTCNT - 1) {
+					if (selectedStage == 1) kirby.centerPos = { stage12[moveIndex].x, stage12[moveIndex].y - kirby.bitmapSize.y / 2 };
+					else kirby.centerPos = { stage23[moveIndex].x, stage23[moveIndex].y - kirby.bitmapSize.y / 2 };
+					if (cx < stageBackground.bitmapSize.x - SCREENWIDTH) cx += Movedir * 10;
+					moveIndex++;
+					//printf("%d : %d, %d\n", moveIndex, stage23[moveIndex].x, stage23[moveIndex].y);
+				}
+				else {
+					if (selectedStage == 1) nowStage = 1;
+					else nowStage = 2;
+					Move = FALSE;
+				}
+			}
+			else if (Movedir == -1) {
+				if (moveIndex > 0) {
+					if (selectedStage == 0) kirby.centerPos = { stage12[moveIndex].x, stage12[moveIndex].y - kirby.bitmapSize.y / 2 };
+					else kirby.centerPos = { stage23[moveIndex].x, stage23[moveIndex].y - kirby.bitmapSize.y / 2 };
+					if (cx > 0) cx += Movedir * 10;
+					moveIndex--;
+				}
+				else {
+					if (selectedStage == 0) nowStage = 0;
+					else nowStage = 1;
+					Move = FALSE;
+				}
+			}
+		}
+
+
+		oldBitmap2 = (HBITMAP)SelectObject(memdc2, kirby.hBitmap);
+		kirby.drawPos = { kirby.centerPos.x - kirby.bitmapSize.x / 2, kirby.centerPos.y - kirby.bitmapSize.y / 2 };
+		TransparentBlt(memdc1, kirby.drawPos.x - cx, kirby.drawPos.y - cy, kirby.bitmapSize.x, kirby.bitmapSize.y, memdc2, 0, 0, kirby.bitmapSize.x, kirby.bitmapSize.y, kirbyRGB);
+
+		SelectObject(memdc2, oldBitmap2);
+		DeleteDC(memdc2);
+		SelectObject(memdc1, oldBitmap1);
+		DeleteDC(memdc1);
+		ReleaseDC(hwnd, hdc);
+		InvalidateRgn(hwnd, NULL, false);
+		break;
+
 	case WM_PAINT:
+		GetClientRect(hwnd, &crt);
 		hdc = BeginPaint(hwnd, &ps);
+		memdc1 = CreateCompatibleDC(hdc);
+		oldBitmap1 = (HBITMAP)SelectObject(memdc1, hBitmap);
+		BitBlt(hdc, 0, 0, SCREENWIDTH, SCREENHEIGHT, memdc1, 0, 0, SRCCOPY);
+		SelectObject(memdc1, oldBitmap1);
+		DeleteDC(memdc1);
 		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_CHAR:
+		if(!Move){
+			switch (wParam)
+			{
+				//카메라 좌우 스크롤
+			case 'a':
+				if (cx > 0) cx -= 5;
+				break;
+
+			case 'd':
+				if (cx < stageBackground.bitmapSize.x - SCREENWIDTH) cx += 5;
+				break;
+			}
+		}
+		break;
+
+	case WM_LBUTTONDOWN:
+		//스테이지 선택
+		mx = LOWORD(lParam);
+		my = HIWORD(lParam);
+		for (i = 0; i < STAGECNT; ++i) {
+			if (InCircle({ stageButton[i].centerPos.x - cx, stageButton[i].centerPos.y - cy }, { mx, my })) {
+				selectedStage = i;
+				if (selectedStage < nowStage) {
+					moveIndex = POINTCNT - 1;
+					Movedir = -1;
+				}
+				else if (selectedStage > nowStage) {
+					moveIndex = 0;
+					Movedir = 1;
+				}
+				else  Movedir = 0;
+				Move = TRUE;
+				SetTimer(hwnd, 2, 100, NULL);
+			}
+		}
 		break;
 
 	case WM_DESTROY:
@@ -50,4 +199,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		return 0;
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+BOOL InCircle(POINT a, POINT b) {
+	float dis;		//두 점 간의 길이
+	dis = sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+	if (dis < BUTTONRADIUS) return TRUE;
+	else return FALSE;
 }
--- KirbySmash/KirbySmash/StageScene.h
+++ KirbySmash/KirbySmash/StageScene.h
@@ -1,17 +1,59 @@
 #include <windows.h>
-#include <tchar.h>
+#include <stdio.h>
+#include <math.h>
 
 #ifdef UNICODE
 #pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
 #else
 #pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
 #endif
+#pragma comment (lib, "msimg32.lib")
 
-#define WIDTH 600
-#define HEIGHT 800
+#define WIDTH 496
+#define HEIGHT 839
+#define SCREENWIDTH 480
+#define SCREENHEIGHT 800
+#define STAGECNT 3				//스테이지 개수
+#define BUTTONRADIUS 35			//스테이지 버튼 반지름
+#define POINTCNT 21
 
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = "Window Class Name";
 LPCTSTR lpszWindowName = "Window Program";
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+//함수
+BOOL InCircle(POINT a, POINT b);		//마우스 좌표가 원 안에 있는지 체크
+
+COLORREF buttonRGB = RGB(201, 206, 181);
+COLORREF kirbyRGB = RGB(136, 227, 148);
+
+int i;	//for문 용
+static int cx, cy;			//카메라 좌표
+static int mx, my;			//마우스 좌표
+static int nowStage;		//현재 스테이지
+static int selectedStage;	//선택된 스테이지
+static int moveIndex;		//이동 좌표 인덱스
+static BOOL Move;			//캐릭터 이동
+static int Movedir;			//캐릭터 이동 방향
+
+POINT stage12[POINTCNT] ={
+	{150, 400}, {160, 405}, {170, 410}, {180, 415}, {190, 420},
+	{200, 424}, {210, 427}, {222, 431}, {232, 432}, {244, 432},
+	{255, 430}, {266, 426}, {275, 420}, {284, 416}, {290, 410},
+	{298, 406}, {305, 400}, {313, 394}, {320, 388}, {330, 382}, {338, 378} };
+POINT stage23[POINTCNT] = {
+	{350, 370}, {362, 367}, {376, 367}, {390, 366}, {406, 365},
+	{422, 372}, {435, 378}, {450, 383}, {466, 386}, {482, 387},
+	{498, 388}, {512, 388}, {526, 386}, {540, 384}, {562, 380}, {576, 372} };
+
+struct Image {
+	HBITMAP hBitmap;
+	POINT bitmapSize;	//비트맵 크기
+	POINT drawPos;		//그릴 위치
+	POINT centerPos;	//중앙 위치
+};
+Image stageBackground;
+Image kirby;
+Image stageButton[3];
